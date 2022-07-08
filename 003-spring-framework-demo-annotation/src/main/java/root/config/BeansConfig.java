a58421926f80a958310ecd7ce250864ff2d72959
Yikai Cao <yikaicao27@gmail.com>
2022-07-08 14:59:48 +0800

    [ADDED] annotation based autowiring demo.

---
+++ 003-spring-framework-demo-annotation/src/main/java/root/config/BeansConfig.java
@@ -0,0 +1,11 @@
+package root.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class BeansConfig {
+
+}
+
---
+++ 003-spring-framework-demo-annotation/src/main/java/root/entity/Car.java
@@ -0,0 +1,27 @@
+package root.entity;
+
+public class Car {
+    String brand;
+    int price;
+
+    public Car(String brand, int price) {
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}

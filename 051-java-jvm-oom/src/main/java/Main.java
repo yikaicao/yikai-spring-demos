4af60919741a5a41b8ea1e00ca4f2d820c6c5cea
Yikai Cao <yikaicao27@gmail.com>
2022-07-18 11:44:24 +0800

    [ADDED] java oom demo.

---
+++ 051-java-jvm-oom/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new ArrayList<>();
+        int i = 0;
+        while (true) {
+            i++;
+            list.add(new Object());
+            System.out.println(i);
+        }
+    }
+}

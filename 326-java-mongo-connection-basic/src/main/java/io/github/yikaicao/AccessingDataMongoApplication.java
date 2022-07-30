598ee0535a4b76366081671549ff2bd657a3ad80
Yikai Cao <yikaicao27@gmail.com>
2022-07-30 17:33:40 +0800

    [UPDATED] main function.

---
+++ 326-java-mongo-connection-basic/src/main/java/io/github/yikaicao/AccessingDataMongoApplication.java
@@ -0,0 +1,11 @@
+package io.github.yikaicao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMongoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataMongoApplication.class, args);
+    }
+}

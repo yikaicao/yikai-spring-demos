2cfbcfa19cbce0d88f4d191fc505f7ccedc7935d
Yikai Cao <yikaicao27@gmail.com>
2022-07-30 17:30:02 +0800

    [UPDATED] customer repository.

---
+++ 326-java-mongo-connection-basic/src/main/java/io/github/yikaicao/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package io.github.yikaicao.repository;
+
+import io.github.yikaicao.entity.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+}
